725fac516ffda5f15415dcf41400cdc87deaa341
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2019-05-30 23:25:36 +0300

    -

---
+++ turret.rb
@@ -0,0 +1,64 @@
+require 'rubyserial'
+require 'artoo'
+
+connection :joystick, :adaptor => :joystick
+device :joystick, :driver => :joystick, :connection => :joystick, :interval => 0.05
+
+def read_grbl
+  response = ""
+  begin
+    r = $grbl.read(10)
+    response << r
+  end until r == ""
+  response
+end
+
+def normalize(val)
+  if val < -8192
+    val + 8192
+  elsif val >= -8192 and val <= 8192
+    0
+  else
+    val - 8192
+  end
+end
+
+def sign(n)
+  n <=> 0
+end
+
+$grbl = Serial.new '/dev/cu.SLAB_USBtoUART', 115200
+# $grbl = Serial.new '/dev/cu.usbmodem14201', 115200
+$grbl.write("$$\n")
+sleep 0.1
+
+puts read_grbl
+
+work do
+  on joystick, :joystick => proc {|caller, value|
+    # puts 'joystick ' + value[:s].to_s, value[:x], value[:y]
+    if value[:s].zero?
+      # puts value
+      x = normalize(value[:x])
+      y = normalize(value[:y])
+      # x_smooth = x * 90.0 / 24576
+      # y_smooth = y * 90.0 / 24576
+      # p [x, y]
+      f = Math.sqrt(x ** 2 + y ** 2)
+      command = "$J=G91 X#{-30 * x / 24576} Y#{30 * y / 24576} F#{f*2.0}\n"
+      p command
+      $grbl.write command unless f.zero?
+      # sleep 0.03
+      # response = read_grbl.gsub("ok", '')
+      # print response
+
+      $grbl.write "?\n"
+      sleep 0.03
+      response = read_grbl.gsub("ok", '')
+      print response
+    end
+  }
+  on joystick, :button => proc {|*value|
+    puts 'button ' + value[1].to_s
+  }
+end
